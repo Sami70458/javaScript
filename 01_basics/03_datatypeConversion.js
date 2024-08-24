37e17a726311e7c516feeed94c634a004de4d755
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2024-08-24 11:04:31 +0000

    conversion

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+let scoreNumber = Number (score)
+
+console.log(typeof scoreNumber)
+console.log(scoreNumber)
+
+//33 => 33
+//33abc => NaN
+//null => 0
+//undefined => Nan
+//"hitesh" => NaN
+//true => 1
+
+let isLogged = 1
+let booleanisLogged = Boolean(isLogged)
+console.log(typeof booleanisLogged)
+console.log(booleanisLogged)
+// 0 => false
+//1 => true
+// hitesh => true
+// "" => false
+//null = false
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber)
