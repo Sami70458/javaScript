003a7326972d707975661e7de9b595f5ba42ec98
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2024-08-24 11:18:41 +0000

    chnage

--- 01_basics/03_datatypeConversion.js
+++ 01_basics/03_datatypeConversion.js
@@ -27,3 +27,30 @@ console.log(booleanisLogged)
 let somenumber = 33
 let stringnumber = String(somenumber)
 console.log(stringnumber)
+
+
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " Samiksha"
+let str3 = str1+str2
+console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log("1"+2)
+
+console.log(true)
+console.log(+true)
