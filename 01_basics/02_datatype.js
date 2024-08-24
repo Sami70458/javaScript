14687e036fbd8d6bc1c23dcd88299efa490428c7
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2024-08-24 10:49:21 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using node js, not browser
+
+let name = "Samiksha"
+let age = 20
+let isSingle = true
+
+console.table([name,age,isSingle])
+
+//DATATYPES
+// number
+//bigint
+//string
+//boolean
+//null(standalone value)
+//undefined 
+//symbol => to establish uniqueness
+
+
+
+//object
+
+console.log(typeof age)
+console.log(typeof null)
