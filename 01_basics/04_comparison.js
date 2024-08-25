9d3e8598693a00a89b6291e2514e04461838f864
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2024-08-25 13:40:43 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+console.log(2>1);
+console.log(3==2);
+
+console.log("2"==2); //converts "2" to 2
+
+console.log(null==0);
+console.log(null<0);
+console.log(null>=0); //avoid this kind of conversions
+
+console.log("2"===2);//strict comparison => compares the dataypes also
