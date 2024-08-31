d452efe5244e58b5b86a99b2f9209b67e95356be
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2024-08-31 14:19:01 +0000

    scope

---
+++ 03_basics/03_scope.js
@@ -0,0 +1,13 @@
+
+
+if(true)
+{
+    let a = 10 //work in this given scope only
+    const b = 20 //work in this given scope only
+    var c = 30
+}
+
+console.log(a)
+console.log(b)
+console.log(c)
+
