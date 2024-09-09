f03dcddd6a0362943f362914796897e4e0aa5ec7
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2024-09-09 17:04:02 +0000

    function

---
+++ 03_basics/04_scope.js
@@ -0,0 +1,13 @@
+//+++++++++++++++ interesting +++++++++++++
+
+
+function addone(num){
+      return num + 1
+}
+
+addone(4)
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5)) 
