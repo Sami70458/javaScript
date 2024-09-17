9b14594483acdf0c3c6e77cff156e29d2d24954c
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2024-09-17 09:36:17 +0000

    do_while

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// WHILE LOOP
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`)
+//     index+=2;
+// }
+
+//////
+let array = ["apple","banana","pineapple","orange"]
+let arr = 0;
+
+while(arr < array.length){
+    console.log(`Value is ${array[arr]}`)
+    arr++;
+}
+
+//// ++++++ do-while loop +++++++++++
+let score = 1
+
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score<=10);
